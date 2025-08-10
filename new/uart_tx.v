d9d87f2fe46095345ef49189997d15b794bc20e2
XuKanghan <2841083990@qq.com>
2025-08-10 22:51:05 +0800

    Update uart_tx.v

--- new/uart_tx.v
+++ new/uart_tx.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/08/08 20:59:51
-// Design Name: 
-// Module Name: uart_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module uart_tx(
     input clk,
