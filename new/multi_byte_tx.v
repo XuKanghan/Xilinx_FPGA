6f6efdb8f24eb90005174a9f375941a056f2878c
XuKanghan <2841083990@qq.com>
2025-08-10 22:50:51 +0800

    Update multi_byte_tx.v

--- new/multi_byte_tx.v
+++ new/multi_byte_tx.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/08/08 20:59:25
-// Design Name: 
-// Module Name: multi_byte_tx
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module multi_byte_tx (
     input clk,
