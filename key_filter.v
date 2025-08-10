ea2f4aee657129958a4fe4404df7d113bf837ab9
XuKanghan <2841083990@qq.com>
2025-08-10 22:43:38 +0800

    Update key_filter.v

--- key_filter.v
+++ key_filter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/08/09 22:34:16
-// Design Name: 
-// Module Name: key_filter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module key_filter(
     input clk, //50MHz时钟
